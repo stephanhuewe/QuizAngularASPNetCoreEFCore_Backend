6751f2612204872a587c241a56af1441c3140f83
Stephan Hüwe <stephanhuewe@schaeferhunde.de>
2020-05-10 14:29:24 +0200

    Initial Checkin of backend

---
+++ quiz-backend/Controllers/QuestionController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using quiz_backend.Models;
+
+namespace quiz_backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase {
+        private readonly QuizContext context;
+        public QuestionController(QuizContext quizContext) {
+            context = quizContext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Question question) {
+
+            var quiz = context.Quiz.SingleOrDefault(q => q.Id == question.QuizId);
+
+            if (quiz == null) {
+                return NotFound();
+            }
+            context.Questions.Add(question);
+            await context.SaveChangesAsync();
+            return Ok(question);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Question question) {
+            
+            // Not needed because of using Entry
+            // var question = await context.Questions.SingleOrDefaultAsync(q => q.Id == id);
+            if (id != question.Id) {
+                return BadRequest();
+            }
+
+                context.Entry(question).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(question);
+        }
+
+        [HttpGet]
+        public IEnumerable<Question> Get() {
+            return context.Questions;
+        }
+
+
+        [HttpGet("{quizId}")]
+        public IEnumerable<Question> Get([FromRoute] int quizId) {
+            return context.Questions.Where(q => q.QuizId == quizId);
+        }
+
+    }
+}
---
+++ quiz-backend/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace quiz_backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors(options => options.AddPolicy("Cors", builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }));
+
+            services.AddDbContext<QuizContext>(opt => opt.UseInMemoryDatabase("quiz"));
+            services.AddDbContext<UserDbContext>(opt => opt.UseInMemoryDatabase("user"));
+
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<UserDbContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("Cors");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
